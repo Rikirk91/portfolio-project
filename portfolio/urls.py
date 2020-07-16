537f4f3c0b5bb8b8f47ac0dbe349f14da2427091
RikirkPJ <52795591+Rikirk91@users.noreply.github.com>
2020-07-16 10:32:17 -0500

    blogs and images running on bootstrap

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -18,9 +18,10 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.http import HttpResponse
+import jobs.views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', admin.site.urls),
+    path('', jobs.views.home, name='home'),
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
